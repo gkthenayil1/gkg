631fd78a7fbb96a4fb118eb9977261e65bad4d0a
gkthenayil1 <119516545+gkthenayil1@users.noreply.github.com>
2022-12-01 00:13:53 +0530

    Update Simple.py

--- Simple.py
+++ Simple.py
@@ -9,9 +9,9 @@ import pandas as pd
 import numpy as np
 def div(a, b):
   return a / b
-b=1
+
 st.title('Simple')
-a = st.number_input(' a number')
+a = st.number_input('Enter a number')
 b = st.number_input('Enter another number')
 st.write(div(a, b))
 
