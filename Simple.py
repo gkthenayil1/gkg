347b09c5fc20375290c4e04df5f663ade5d0c493
gkthenayil1 <119516545+gkthenayil1@users.noreply.github.com>
2022-12-06 20:54:15 +0530

    Update Simple.py

--- Simple.py
+++ Simple.py
@@ -13,7 +13,7 @@ def div(a, b):
 st.title('Simple division')
 a = st.number_input('Enter a number')
 b = st.number_input('Enter another number',value=1.00)
-st.write("your answer is "div(a, b))
+st.write("your answer is ",div(a, b))
 
 
 
